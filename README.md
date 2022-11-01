a3a89287f0330bcdc6a6cbaa7fcdf2705dbcd231
bakks <pbbakkum@gmail.com>
2022-10-31 17:14:41 -0700

    Getting things running again

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 ```
 sudo apt-get update
 sudo apt-get install golang imagemagick libmagickwand-dev
+
+sudo sed -i '/PDF/d' /etc/ImageMagick-6/policy.xml
+
+cd ~                  
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.71.tar.gz                       
+tar xvfz bcm2835-1.71.tar.gz;                      
+cd bcm2835-1.71;                       
+./configure;                      
+make;        
+sudo make install
 ```
--- bin/run.sh
+++ bin/run.sh
@@ -3,8 +3,10 @@ set -e
 
 #OUTPUT=$(./fetch | tail -1)
 d=$(TZ='America/Los_Angeles' date +'%Y/%m/%d')
+#rm -f nytimes.bmp
 rm -f scan.pdf nytimes.bmp
-wget https://static01.nyt.com/images/${d}/nytfrontpage/scan.pdf
-convert -density 200 scan.pdf -crop 2360x4310+40+190 -resize x1872 -rotate 270 nytimes.bmp
+#wget https://static01.nyt.com/images/${d}/nytfrontpage/scan.pdf
+#convert -density 200 scan.pdf -crop 2360x4310+40+190 -resize x1872 -rotate 270 nytimes.bmp
+convert -density 200 scan.pdf -crop 2360x3700+40+190 -resize x1872 -rotate 270 nytimes.bmp
 ./display -1.14 nytimes.bmp
 
