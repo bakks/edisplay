fdc71041826320fde86368d07de74c8284a7a01a
Peter Bakkum <pbbakkum@gmail.com>
2022-11-01 12:49:14 -0700

    Clean up text demo

--- py/code.py
+++ py/code.py
@@ -1,36 +1,13 @@
-import board
-import digitalio
-import rotaryio
-import neopixel
-import keypad
 import random
 from rainbowio import colorwheel
-
 from adafruit_macropad import MacroPad
 
 macropad = MacroPad()
-
 text_lines = macropad.display_text(title="MacroPad Info")
-
-
-
-#key_pins = (board.KEY1, board.KEY2, board.KEY3, board.KEY4, board.KEY5, board.KEY6,
-#            board.KEY7, board.KEY8, board.KEY9, board.KEY10, board.KEY11, board.KEY12)
-#keys = keypad.Keys(key_pins, value_when_pressed=False, pull=True)
-
-#encoder = rotaryio.IncrementalEncoder(board.ROTA, board.ROTB)
-#button = digitalio.DigitalInOut(board.BUTTON)
-#button.switch_to_input(pull=digitalio.Pull.UP)
-
-#pixels = neopixel.NeoPixel(board.NEOPIXEL, 12, brightness=0.8)
 last_position = None
 
 while True:
     different = False
-#    if not button.value:
-#        pixels.brightness = 1.0
-#    else:
-#        pixels.brightness = 0.5
 
     position = macropad.encoder
     if last_position is None or position != last_position:
